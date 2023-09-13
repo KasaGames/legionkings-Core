bd8328388d6aead081e24d9fdd94a7d076ee0ce4
Manuel Lezcano <62978637+kasanova54@users.noreply.github.com>
2023-09-13 18:22:10 +0100

    Update Chat.cpp

--- src/server/game/Chat/Chat.cpp
+++ src/server/game/Chat/Chat.cpp
@@ -3,7 +3,7 @@
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
+ * under the terms of the GNU General Publiqwc License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
